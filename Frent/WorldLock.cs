427cf69b3fa17555f66c8306f72a4841a3c23328
f300xen <33439155+f300xen@users.noreply.github.com>
2025-07-15 23:16:43 -0700

    Added public locking mechanism

---
+++ Frent/WorldLock.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Frent;
+
+/// <summary>
+/// A disposable struct that safely manages a lock on the World to allow for
+/// manual structural changes. Guarantees that the world is properly unlocked.
+/// </summary>
+public readonly struct WorldLock : IDisposable
+{
+	private readonly World _world;
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	internal WorldLock(World world)
+	{
+		_world = world;
+		_world.EnterDisallowState();
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void Dispose()
+	{
+		_world.ExitDisallowState(null, _world.CurrentConfig.UpdateDeferredCreationEntities);
+	}
+}
+
+public partial class World
+{
+	/// <summary>
+	/// Acquires a temporary, exclusive lock on the World.
+	/// </summary>
+	/// <returns>An IDisposable WorldLock object.</returns>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public WorldLock Lock()
+	{
+		return new WorldLock(this);
+	}
+}
--- build.ps1
+++ build.ps1
@@ -1,63 +1,17 @@
-# normal package
+# All publishing and Unity-specific steps have been removed.
 
-# build strategy
-# delete bin -> build generator -> copy generator -> build frent -> push
-# detete bin -> build generator -> copy generator -> tmp csproj -> build frent.unity -> delete tmp csproj -> push
+echo "--- Cleaning old build artifacts ---"
+Remove-Item Frent\bin\Release\* -Recurse -ErrorAction SilentlyContinue
 
-echo "Building Generator"
-
-rm Frent\bin\Release\* -Recurse
-
+echo "--- Building Source Generator ---"
 dotnet build -c Release Frent.Generator\Frent.Generator.csproj
 
+echo "--- Copying Source Generator to main project ---"
 Copy-Item -Path ".\Frent.Generator\bin\Release\netstandard2.0\Frent.Generator.dll" -Destination ".\Frent\bin\Release\"
 
-echo "Building Frent"
+echo "--- Building Frent Library ---"
+dotnet build -c Release Frent\Frent.csproj
 
-dotnet build -c Release Frent\Frent.csproj /p:Publish=true
-
-$package = Get-ChildItem -Path ".\Frent\bin\Release" -Filter "*.nupkg" | Select-Object -First 1
-
-echo "Pushing package $package"
-
-pause
-
-dotnet nuget push ".\Frent\bin\Release\$package" --api-key $Env:NUGET_API_KEY --source https://api.nuget.org/v3/index.json
-
-pause
-
-clear
-
-# unity package
-
-rm Frent\bin\Release\* -Recurse
-
-echo "Building Unity Generator"
-
-dotnet build -c Release Frent.Generator\Frent.Generator.csproj /p:Unity="#UNITY"
-
-Copy-Item -Path ".\Frent.Generator\bin\Release\netstandard2.0\Frent.Generator.dll" -Destination ".\Frent\bin\Release\"
-
-echo "Building Frent.Unity"
-
-# copy tmp csproj
-Copy-Item -Path ".\Frent\Frent.csproj" -Destination ".\Frent\Frent.Unity.csproj"
-
-dotnet build -c Release Frent\Frent.Unity.csproj /p:Publish=true
-
-# cleanup
-rm ".\Frent\Frent.Unity.csproj"
-
-$package = Get-ChildItem -Path ".\Frent\bin\Release" -Filter "*.nupkg" | Select-Object -First 1
-
-echo "Pushing package $package"
-
-pause
-
-dotnet nuget push ".\Frent\bin\Release\$package" --api-key $Env:NUGET_API_KEY --source https://api.nuget.org/v3/index.json
-
-pause
-
-echo "DONE! Don't forget to update docs!!!!"
-
-pause
+echo ""
+echo "--- BUILD COMPLETE! ---"
+echo "Your custom Frent.dll is located in: .\Frent\bin\Release\"

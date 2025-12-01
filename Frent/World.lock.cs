6785846cb4117294f204c32a7e8c22b7ec8830cc
f300xen <33439155+f300xen@users.noreply.github.com>
2025-11-30 16:01:05 -0800

    re-added world lock

---
+++ Frent/World.lock.cs
@@ -0,0 +1,28 @@
+﻿namespace Frent;
+
+public partial class World
+{
+	/// <summary>
+	/// Manually locks the world. Structural changes (entity creation/deletion, component addition/removal)
+	/// occurring within the scope will be deferred until the returned object is disposed.
+	/// </summary>
+	/// <returns>An disposable object that releases the lock.</returns>
+	public WorldLock Lock() => new(this);
+	/// <summary>
+	/// A disposable structure that maintains a lock on the World.
+	/// </summary>
+	public readonly struct WorldLock : IDisposable
+	{
+		private readonly World _world;
+		internal WorldLock(World world)
+		{
+			_world = world;
+			_world.EnterDisallowState();
+		}
+		/// <inheritdoc/>
+		public void Dispose()
+		{
+			_world.ExitDisallowState(null, updateDeferredEntities: false);
+		}
+	}
+}
